01b860bb65914ddb3ae5f3a9048d615ebc43195e
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-17 18:31:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Core |Concepts 2</h1>
+    </>
+  )
+}
+
+export default App
